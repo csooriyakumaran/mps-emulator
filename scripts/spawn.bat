22cc844219b692bcc0b7780612add39f32fbe5e0
csooriyakumaran <58654149+csooriyakumaran@users.noreply.github.com>
2026-04-08 15:15:08 -0400

    updated spawn.bat to follow new target name mps-emulator

--- scripts/spawn.bat
+++ scripts/spawn.bat
@@ -1,10 +1,10 @@
 @echo off
-start "MPS-01" .\bin\Release-windows-x86_64\mps-server\mps-server.exe --bind-ip 127.0.0.12 --port 23 --type 4264
-start "MPS-02" .\bin\Release-windows-x86_64\mps-server\mps-server.exe --bind-ip 127.0.0.13 --port 23 --type 4264 
-start "MPS-03" .\bin\Release-windows-x86_64\mps-server\mps-server.exe --bind-ip 127.0.0.14 --port 23 --type 4264
-start "MPS-04" .\bin\Release-windows-x86_64\mps-server\mps-server.exe --bind-ip 127.0.0.15 --port 23 --type 4232
-start "MPS-05" .\bin\Release-windows-x86_64\mps-server\mps-server.exe --bind-ip 127.0.0.16 --port 23 --type 4232
-start "MPS-06" .\bin\Release-windows-x86_64\mps-server\mps-server.exe --bind-ip 127.0.0.17 --port 23 --type 4216
-start "MPS-07" .\bin\Release-windows-x86_64\mps-server\mps-server.exe --bind-ip 127.0.0.18 --port 23 --type 4216
-start "MPS-08" .\bin\Release-windows-x86_64\mps-server\mps-server.exe --bind-ip 127.0.0.19 --port 23 --type 4216
+start "MPS-01" mps-emulator.exe --bind-ip 127.0.0.12 --port 23 --type 4264
+start "MPS-02" mps-emulator.exe --bind-ip 127.0.0.13 --port 23 --type 4264 
+start "MPS-03" mps-emulator.exe --bind-ip 127.0.0.14 --port 23 --type 4264
+start "MPS-04" mps-emulator.exe --bind-ip 127.0.0.15 --port 23 --type 4232
+start "MPS-05" mps-emulator.exe --bind-ip 127.0.0.16 --port 23 --type 4232
+start "MPS-06" mps-emulator.exe --bind-ip 127.0.0.17 --port 23 --type 4216
+start "MPS-07" mps-emulator.exe --bind-ip 127.0.0.18 --port 23 --type 4216
+start "MPS-08" mps-emulator.exe --bind-ip 127.0.0.19 --port 23 --type 4216
 
